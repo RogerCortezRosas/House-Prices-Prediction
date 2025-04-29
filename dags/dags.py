e19a833d0127f6bc81cb0000a65e13cfe485ac27
Rogelio Cortez <rcortezrosas@gmail.com>
2025-04-29 16:20:20 -0600

    up

--- dags/dags.py
+++ dags/dags.py
@@ -3,6 +3,16 @@ from airflow.operators.bash import BashOperator
 from airflow.providers.common.sql.sensors.sql import SqlSensor 
 from airflow.utils.trigger_rule import TriggerRule
 from datetime import datetime, timedelta
+import sys
+from airflow.operators.python_operator import PythonOperator
+
+
+from pathlib import Path
+
+# Añade el directorio Proyecto/ al path de Python
+sys.path.append(str(Path(__file__).parent.parent))
+
+from scripts.transform import Transform_Python
 
 
 with DAG( dag_id = 'House_prices_ETL',description = 'DAG tht make etl' , schedule = '@daily',start_date = datetime(2023,10,1),
@@ -15,10 +25,10 @@ with DAG( dag_id = 'House_prices_ETL',description = 'DAG tht make etl' , schedul
 
     #Task 2 : Check if there is new data
     check_new_data = SqlSensor( task_id = 'check_new_data', conn_id ='mysql_default',
-                                sql = 'SELECT COUNT(*) FROM house_prices WHERE created_at > NOW() - INTERVAL 1 DAY', #Secciona los registros que fueron creados o actualizados en las últimas 24 horas.
+                                sql = 'SELECT COUNT(*) FROM house_DataLake WHERE created_at > NOW() - INTERVAL 1 DAY', #Secciona los registros que fueron creados o actualizados en las últimas 24 horas.
                                 mode = 'reschedule', timeout = 600, poke_interval = 60, soft_fail = False, trigger_rule = TriggerRule.ALL_SUCCESS)
     
     #Task 3 : Transform data with Python
-    transform_data_Python = BashOperator( task_id = 'transform_data', bash_command = "sleep 5 && echo 'Transforming data with PySpark'")
+    transform_data_Python = Transform_Python( task_id = 'transform_data_Python',depends_on_past = False, trigger_rule = TriggerRule.ALL_SUCCESS)
 
     satrt_message >> check_new_data >> transform_data_Python
