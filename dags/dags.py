ab008ca60818b810a3d43e0cab6df020954de04e
Rogelio Cortez <rcortezrosas@gmail.com>
2025-04-30 11:43:45 -0600

    UP

--- dags/dags.py
+++ dags/dags.py
@@ -21,11 +21,11 @@ with DAG( dag_id = 'House_prices_ETL',description = 'DAG tht make etl' , schedul
     # Define tasks
 
     #Task 1: Message to indicates the start of th DAG
-    satrt_message = BashOperator( task_id = 'start_message', bash_command = "sleep 5 && echo 'ETL starting")
+    satrt_message = BashOperator( task_id = 'start_message', bash_command = 'sleep 5 && echo "ETL starting"')
 
     #Task 2 : Check if there is new data
     check_new_data = SqlSensor( task_id = 'check_new_data', conn_id ='mysql_default',
-                                sql = 'SELECT COUNT(*) FROM house_DataLake WHERE created_at > NOW() - INTERVAL 1 DAY', #Secciona los registros que fueron creados o actualizados en las últimas 24 horas.
+                                sql = 'SELECT COUNT(*) FROM house_DataLake WHERE COUNT(id) > 1', #Secciona los registros que fueron creados o actualizados en las últimas 24 horas.
                                 mode = 'reschedule', timeout = 600, poke_interval = 60, soft_fail = False, trigger_rule = TriggerRule.ALL_SUCCESS)
     
     #Task 3 : Transform data with Python
